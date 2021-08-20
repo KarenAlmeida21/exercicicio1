b71d88c65c852c30575f0eb6386dd21549abf4f2
Karen Almeida <karenalmeida61@hotmail.com>
2021-08-20 15:13:19 -0300

    adicionando times na serie a

--- clubes.bat
+++ clubes.bat
@@ -2,3 +2,6 @@ cd "clubes"
 md "serie a"
 cd..
 md "serie b"
+cd..
+md "Atlético-MG" "Palmeiras" "Fortaleza" "Bragantino" "Flamengo" "Athletico-PR" "Atlético-GO" "Ceará" "Internacional" "Santos" "Corinthians" "Juventude" "Bahia"
+"São Paulo" "Fluminense" "Cuiabá" "Sport" "América-MG" "Grêmio" "Chapecoense"
