e71e93d9eb5df93753a4806c775719289553c2a2
Karen Almeida <karenalmeida61@hotmail.com>
2021-08-20 15:21:21 -0300

    adicionando times na serie b

--- clubes.bat
+++ clubes.bat
@@ -5,3 +5,8 @@ md "serie b"
 cd..
 md "Atlético-MG" "Palmeiras" "Fortaleza" "Bragantino" "Flamengo" "Athletico-PR" "Atlético-GO" "Ceará" "Internacional" "Santos" "Corinthians" "Juventude" "Bahia"
 "São Paulo" "Fluminense" "Cuiabá" "Sport" "América-MG" "Grêmio" "Chapecoense"
+cd ..
+cd "serie b"
+md "Coritiba" "Goiás" "Avaí" "CRB" "Sampaio Corrêa" "Guarani" "Náutico" "Botafogo" "Operário-PR" "CSA" "Vasco" "Remo" "Brusque" "Cruzeiro" "Vila Nova" "Ponte Preta"
+"Londrina" "Vitória" "Confiança" "Brasil de Pelotas"
+
